0c20e398def9f1be66630cc0e6ad7c64be0991eb
MohamedJalalMounadi <44229654+MohamedJalalMounadi@users.noreply.github.com>
2019-08-24 00:04:48 +0100

    ok3

--- Le PFE/PFE/Model.py
+++ Le PFE/PFE/Model.py
@@ -16,3 +16,32 @@ class Client(DB.Model, UserMixin):
     Email = DB.Column(DB.String(120), nullable=False)
     Login = DB.Column(DB.String(20), nullable=False)
     MotDePass = DB.Column(DB.String(60), nullable=False)
+    Compte = DB.relationship('Compte', backref='Client', lazy=True)
+
+
+class Agence(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    Responsable = DB.Column(DB.String(100), nullable=False)
+    ville = DB.Column(DB.String(120), nullable=False)
+    CodeGuichet = DB.Column(DB.Integer, nullable=False)
+    Fax = DB.Column(DB.String(100, nullable=False)
+    Conseiller = DB.relationship('Conseiller', backref='Agence', lazy=True)
+
+
+class Compte(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    Numero = DB.Column(DB.Integer, nullable=False)
+    Solde = DB.Column(DB.Float, nullable=False)
+    DateOuverture = DB.Column(DB.DateTime, nullable=False)
+    client_id = DB.Column(DB.Integer,
+                          DB.ForeignKey('client.id'), nullable=False)
+
+class Conseiller(DB.Model):
+    id =  DB.Column(DB.Integer, primary_key=True)
+    Nom = DB.Column(DB.String(40), nullable=False)
+    Prenom = DB.Column(DB.String(40), nullable=False)
+    DateNaissance = DB.Column(DB.String(20), nullable=False)
+    Telephone = DB.Column(DB.String(20), nullable=False)
+    Email = DB.Column(DB.String(120), nullable=False)
+    agence_id = DB.Column(DB.Integer,
+                          DB.ForeignKey('agence.id'), nullable=False)
--- Le PFE/PFE/route.py
+++ Le PFE/PFE/route.py
@@ -1,19 +1,23 @@
 from flask import render_template, flash, redirect, url_for
 from PFE import APP, bcrypt
 from PFE.Forms import LoginForm
-from PFE.Model import Client
+from PFE.Model import Client, Compte
+from flask_login import login_user, current_user, logout_user
+import datetime
+import locale
 
 
 @APP.route("/", methods=['GET', 'POST'])
-def hello():
-
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('situation_financiere'))
     form = LoginForm()
     if form.validate_on_submit():
         client = Client.query.filter_by(Login=form.login.data).first()
         if client and bcrypt.check_password_hash(
                     client.MotDePass, form.mdp.data):
-            flash(f'connexion succeed  {form.login.data} ! ', 'success')
-            return redirect(url_for('home'))
+            login_user(client, remember=form.remember.data)
+            return redirect(url_for('situation_financiere'))
         else:
             flash(f'login or password error ! ', 'danger')
     return render_template('Authentification.html', form=form)
@@ -24,6 +28,15 @@ def home():
     return render_template('situation_financiere.html')
 
 
+@APP.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
 @APP.route("/situation_financiere")
 def situation_financiere():
-    return render_template('situation_financiere.html')
+    compt = Compte.query.filter_by(client_id=current_user.id).first()
+    locale.setlocale(locale.LC_TIME, '')
+    date = datetime.datetime.now()
+    return render_template('situation_financiere.html', cmpt=compt, date=date)
